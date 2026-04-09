f1dce370d7a72de53c9d3a74a85d435a4ff988ea
Fishcake-x-dingz <rezkyirawan.95@gmail.com>
2026-04-09 11:06:14 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,13 @@
+window.DB = {
+  machines: [
+    { name:"Jura E6", brand:"Jura", price:15000000, power:1450, madeIn:"Swiss" },
+    { name:"Delonghi ECAM 350", brand:"Delonghi", price:17000000, power:1350, madeIn:"Italy" },
+    { name:"Kalerm X465", brand:"Kalerm", price:12000000, power:1500, madeIn:"China" }
+  ],
+
+  grinders: [
+    { name:"Mazzer Mini", brand:"Mazzer", price:8000000, power:350, madeIn:"Italy" },
+    { name:"Eureka Mignon", brand:"Eureka", price:7000000, power:310, madeIn:"Italy" },
+    { name:"Baratza Sette", brand:"Baratza", price:6000000, power:290, madeIn:"USA" }
+  ]
+};
